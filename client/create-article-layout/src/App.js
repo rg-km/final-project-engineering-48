a5860e553abc80bbdb2e08bb9ba955778ec62ff4
alvinfaruq <alvinasshegaff77@gmail.com>
2022-06-19 11:27:41 +0700

    create-article-layout

--- client/create-article-layout/src/App.js
+++ client/create-article-layout/src/App.js
@@ -1,4 +1,9 @@
-import "./App.css";
+//import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 
 import { useForm } from "react-hook-form";
 
@@ -12,8 +17,20 @@ function App() {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-    reset();
+    confirmAlert({
+      title: "Konfirmasi",
+      message: "Apakah anda yakin ingin menyimpan data ini?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => console.log(data),
+        },
+        {
+          label: "No",
+          //onClick: () => alert("Click No"),
+        },
+      ],
+    });
   };
 
   // console.log(watch());
@@ -21,36 +38,47 @@ function App() {
   // console.log(errors.name)
 
   return (
-    <div className="form-page">
-      <div className="Navbar">
-        <ul>
-          <li>
-            <a href="/home">
-              <p>TulisAja</p>
-            </a>
-          </li>
-          <li>
-            <a href="/writing">
-              <p>Writing</p>
-            </a>
-          </li>
-          <li>
-            <a href="/reading">
-              <p>Reading</p>
-            </a>
-          </li>
-          <li>
-            <a href="/about">
-              <p>About</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="container pt-5">
+    <div className="main-wrappe">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-navnew static-top mb-5 shadow">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            TulisAja
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Writing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Reading
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
         <div className="row justify-content-sm-center pt-5">
-          <div className="col-sm-6 shadow round pb-3">
+          <div className="col-sm-6 shadow round pb-3 bg-formnew">
             <h1 className="text-center pt-3 text-secondary">Form Add</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} className="form-inline">
               {/* Judul */}
               <div className="form-group">
                 <label className="col-form-label">Judul :</label>
@@ -117,11 +145,15 @@ function App() {
               </div>
 
               {/* Submit */}
-              <input type="submit" className="btn btn-primary my-3 submit" value="Submit" />
+              <input type="submit" className="btn bg-navnew my-3 submit" value="Submit" />
             </form>
           </div>
         </div>
       </div>
+
+      <footer className="fixed-bottom bg-navnew">
+        <div className="text-center p-3 text-white">© Engineering48 | Ruangguru | 2022</div>
+      </footer>
     </div>
   );
 }
