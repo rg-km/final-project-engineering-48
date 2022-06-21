a32b507d21a474670adf3fd1c8c4bc78d347789f
Ariq Yuda Pratama <ariqyuda22@gmail.com>
2022-06-21 15:09:11 +0700

    add: article publish, arsips & get article list

--- server/api/article.go
+++ server/api/article.go
@@ -15,13 +15,19 @@ type Article struct {
 	Category string `json:"category"`
 }
 
+type ArticleSuccessResponse struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
 type CategorySuccessResponse struct {
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
 
-type ArticleSuccessResponse struct {
-	Articles []Article `json:"Articles"`
+type ArticleResponse struct {
+	Message string  `json:"message"`
+	Data    Article `json:"data"`
 }
 
 func (api *API) getArticleCategory(w http.ResponseWriter, r *http.Request) {
@@ -41,4 +47,79 @@ func (api *API) getArticleCategory(w http.ResponseWriter, r *http.Request) {
 	}
 
 	encoder.Encode(categoryResponse)
+}
+
+func (api *API) publishArticle(w http.ResponseWriter, r *http.Request) {
+	api.AllowOrigin(w, r)
+	var article Article
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	userID := r.Context().Value("id").(int64)
+
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	err = api.articlesRepo.InsertIntoPublish(userID, article.Subject, article.Content, article.Category)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(AuthErrorResponse{Error: err.Error()})
+		return
+	}
+
+	articleResponse := ArticleResponse{
+		Message: "success publish article",
+		Data:    article,
+	}
+
+	encoder.Encode(articleResponse)
+}
+
+func (api *API) arsipArticle(w http.ResponseWriter, r *http.Request) {
+	api.AllowOrigin(w, r)
+	var article Article
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	userID := r.Context().Value("id").(int64)
+
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	err = api.articlesRepo.InsertIntoArsip(userID, article.Subject, article.Content, article.Category)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		encoder.Encode(AuthErrorResponse{Error: err.Error()})
+		return
+	}
+
+	articleResponse := ArticleResponse{
+		Message: "success arsip article",
+		Data:    article,
+	}
+
+	encoder.Encode(articleResponse)
+}
+
+func (api *API) getArticle(w http.ResponseWriter, r *http.Request) {
+	api.AllowOrigin(w, r)
+	article, err := api.articlesRepo.FetchArticle()
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			encoder.Encode(ArticleErrorResponse{Error: err.Error()})
+		}
+	}()
+	articleResponse := ArticleSuccessResponse{
+		Message: "success",
+		Data:    article,
+	}
+
+	encoder.Encode(articleResponse)
 }
--- server/repository/article.go
+++ server/repository/article.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"errors"
+	"time"
 )
 
 type ArticleRepository struct {
@@ -62,4 +63,69 @@ func (u *ArticleRepository) FetchArticleCategory() ([]string, error) {
 	}
 
 	return categories, nil
+}
+
+func (u *ArticleRepository) FetchArticle() ([]Creation, error) {
+	var sqlStmt string
+	var creations []Creation
+
+	// query untuk mengambil category article
+	sqlStmt = `SELECT u.id, u.username, c.id, c.subject, c.category
+	FROM users u
+	LEFT JOIN creations c ON u.id = c.user_id WHERE c.status = "publish"`
+
+	rows, err := u.db.Query(sqlStmt)
+	if err != nil {
+		return nil, errors.New("Error fetching article")
+	}
+	defer rows.Close()
+
+	var creation Creation
+	for rows.Next() {
+		err := rows.Scan(
+			&creation.ID,
+			&creation.UserUsername,
+			&creation.UserID,
+			&creation.Subject,
+			&creation.category,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		creations = append(creations, creation)
+	}
+
+	return creations, nil
+}
+
+func (u *ArticleRepository) InsertIntoPublish(user_id int64, subject, content, category string) error {
+	var sqlStmt string
+	status := "publish"
+
+	// query untuk memasukkan data article ke tabel
+	sqlStmt = `INSERT INTO creations (user_id, subject, content, category, status, created_at) VALUES (?, ?, ?, ?, ?, ?)`
+
+	_, err := u.db.Exec(sqlStmt, user_id, subject, content, category, status, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *ArticleRepository) InsertIntoArsip(user_id int64, subject, content, category string) error {
+	var sqlStmt string
+	status := "arsip"
+
+	// query untuk memasukkan data article ke tabel
+	sqlStmt = `INSERT INTO creations (user_id, subject, content, category, status, created_at) VALUES (?, ?, ?, ?, ?, ?)`
+
+	_, err := u.db.Exec(sqlStmt, user_id, subject, content, category, status, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
