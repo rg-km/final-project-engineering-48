eb85a897dcefbbffef4f250ad8fc8226c6a7abef
alvinfaruq <alvinasshegaff77@gmail.com>
2022-06-27 10:10:00 +0700

    update create article

--- client/src/components/createArticle.js
+++ client/src/components/createArticle.js
@@ -2,6 +2,7 @@ import { confirmAlert } from "react-confirm-alert";
 import { useForm } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
+import axios from "axios";
 
 export default function CreateArticle(props) {
   const {
@@ -19,7 +20,7 @@ export default function CreateArticle(props) {
       buttons: [
         {
           label: "Ya",
-          onClick: () => console.log(data),
+          onClick: () => kirim(data),
         },
         {
           label: "Tidak",
@@ -27,6 +28,30 @@ export default function CreateArticle(props) {
         },
       ],
     });
+  };
+
+  const kirim = (mydata) => {
+    let url = "http://localhost:8080/api/article/publish";
+    let config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+      },
+    };
+    let data = {
+      subject: mydata["judul"],
+      content: mydata["isi"],
+      category: mydata["kategori"],
+    };
+    axios.post(url, data, config).then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   };
 
   const CREATE_LAYOUT = {
@@ -43,7 +68,6 @@ export default function CreateArticle(props) {
 
   return (
     <div className="container">
-      <br></br>
       <div className="row justify-content-sm-center pt-5" style={WRAPPER}>
         <div className="col-sm-6 shadow round pb-3 bg-formnew" style={CREATE_LAYOUT}>
           <h1 className="text-center pt-3 text-secondary">Form Add</h1>
@@ -61,25 +85,6 @@ export default function CreateArticle(props) {
               />
               {errors.judul && <small className="text-danger">{errors.judul.message}</small>}
             </div>
-
-            {/* Kategori */}
-            <label className="col-form-label">Kategori :</label>
-            <select
-              className="form-select form-select-lg mb-3"
-              aria-label=".form-select-lg Kategori"
-              {...register("kategori", { required: "Kategori harus diisi" })}
-              onChange={(e) => {
-                console.log(e.target.value);
-                trigger("kategori");
-              }}
-            >
-              <option value="">Pilih Kategori</option>
-              <option value="science">Science</option>
-              <option value="horror">Horror</option>
-              <option value="technology">Technology</option>
-              <option value="fiction">Fiction</option>
-            </select>
-            {errors.kategori && <small className="text-danger">{errors.kategori.message}</small>}
 
             {/* Isi */}
             <div className="form-group">
@@ -103,6 +108,39 @@ export default function CreateArticle(props) {
               ></textarea>
               {errors.isi && <small className="text-danger">{errors.isi.message}</small>}
             </div>
+
+            {/* Kategori */}
+            {/* <div className="form-group">
+              <label className="col-form-label">Kategori :</label>
+              <input
+                type="text"
+                className={`form-control ${errors.kategori && "invalid"}`}
+                {...register("kategori", { required: "kategori harus diisi" })}
+                onKeyUp={() => {
+                  trigger("kategori");
+                }}
+              />
+              {errors.kategori && <small className="text-danger">{errors.kategori.message}</small>}
+            </div> */}
+
+            {/* Kategori */}
+            <label className="col-form-label">Kategori :</label>
+            <select
+              className="form-select form-select-lg mb-3"
+              aria-label=".form-select-lg Kategori"
+              {...register("kategori", { required: "Kategori harus diisi" })}
+              onChange={(e) => {
+                console.log(e.target.value);
+                trigger("kategori");
+              }}
+            >
+              <option value="">Pilih Kategori</option>
+              <option value="Science">Science</option>
+              <option value="Horror">Horror</option>
+              <option value="Teknologi">Teknologi</option>
+              <option value="Fiksi">Fiksi</option>
+            </select>
+            {errors.kategori && <small className="text-danger">{errors.kategori.message}</small>}
 
             {/* Submit */}
             <input style={BUTTON} type="submit" className="btn bg-navnew my-3 submit" value="Submit" />
